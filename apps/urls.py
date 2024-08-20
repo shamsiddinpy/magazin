e20d6ecd5b21a56a47403df631d8e6a0036a0e89
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-08-20 21:28:47 +0500

    .

--- apps/urls.py
+++ apps/urls.py
@@ -2,16 +2,16 @@ from django.urls import path, include
 from rest_framework import routers
 
 from apps.views import ProductModelViewSet, UserRegistrationView, CartModelViewSet, CartDestroyAPIView, \
-    UserLoginGenericAPIView, PasswordChangeView
+    UserLoginGenericAPIView, PasswordChangeView, CartUpdateAPIView
 
 router = routers.SimpleRouter(False)
 router.register(r'products', ProductModelViewSet, basename='products')
-# router.register(r'cart', CartModelView/Set, basename='cart')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('cart', CartModelViewSet.as_view(), name='cart'),
     path('cart/<int:pk>', CartDestroyAPIView.as_view(), name='cart_id'),
+    path('cart-update<int:pk>', CartUpdateAPIView.as_view(), name='cart_update'),
     path('sign-up', UserRegistrationView.as_view(), name='sign-up'),
     path('login', UserLoginGenericAPIView.as_view(), name='login'),
     path('change-password/', PasswordChangeView.as_view(), name='change-password'),
--- apps/views.py
+++ apps/views.py
@@ -2,7 +2,8 @@ from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.generics import CreateAPIView, ListCreateAPIView, DestroyAPIView, get_object_or_404, GenericAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, DestroyAPIView, get_object_or_404, GenericAPIView, \
+    UpdateAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -28,28 +29,8 @@ class ProductModelViewSet(ModelViewSet):
         return Product.objects.all()
 
 
-@extend_schema(tags=['Register'])
-class UserRegistrationView(CreateAPIView):
-    serializer_class = UserModelSerializer
-    pagination_class = PageNumberPagination
-    permission_classes = (AllowAny,)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            token, created = Token.objects.get_or_create(user=user)
-            headers = self.get_success_headers(serializer.data)
-            return Response({
-                "message": "Foydalanuvchi muvaffaqiyatli ro'yxatdan o'tdi.",
-                "user": serializer.data,
-                "token": token.key
-            }, status=status.HTTP_201_CREATED, headers=headers)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 @extend_schema(tags=['Cart'])
-class CartModelViewSet(ListCreateAPIView):
+class CartModelViewSet(UpdateAPIView):
     serializer_class = CartItemCreateSerializer
 
     def get_queryset(self):
@@ -65,7 +46,26 @@ class CartModelViewSet(ListCreateAPIView):
 
 
 @extend_schema(tags=['Cart'])
+class CartUpdateAPIView(UpdateAPIView):
+    serializer_class = CartItemCreateSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart__user=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        cart_item = self.get_object()
+        serializer = self.get_serializer(cart_item, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        serializer.save(cart_item=cart_item)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@extend_schema(tags=['Cart'])
 class CartDestroyAPIView(DestroyAPIView):
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart__user=self.request.user)
 
     def destroy(self, request, *args, **kwargs):
         cart_item = get_object_or_404(CartItem, pk=kwargs['pk'], cart__user=request.user)
